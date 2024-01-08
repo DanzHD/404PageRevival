e649edff56be0c86f5716e9aa0dc25526724c9c2
Duongdh <danielhangsengduong@gmail.com>
2024-01-09 09:42:47 +1100

    Fixed issues with Content Security Policy and added loading icon

--- 404PageRevival.js
+++ 404PageRevival.js
@@ -1,15 +1,17 @@
-
-
 // ==UserScript==
 // @name         404PageRevival
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  Redirects 404 page not found to the according page in the Wayback Machine
 // @author       Daniel Duong
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
-// @grant        none
-// @licence      MIT
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @resource     MATERIAL_ICON https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined
+// @license      MIT
+
 // ==/UserScript==
 
 const infoBarContainerStyle = 'position: fixed; width: 100%;height: 40px; top: 0; background-color: #333; ' +
@@ -20,20 +22,6 @@ const buttonStyle = "width: fit-content; height: 80%; background-color: orange; 
     "color: white; font-size: 12px; padding: 10px; margin-left: auto; margin-right: 20px; cursor: pointer;";
 const textStyle = "color: white;";
 
-
-let httpMetaTag = document.createElement('meta');
-httpMetaTag.setAttribute('http-equiv', "Content-Security-Policy");
-httpMetaTag.setAttribute('content', "upgrade-insecure-requests");
-document.getElementsByTagName('head')[0].appendChild(httpMetaTag);
-
-let googleIconLinkTag = document.createElement('link');
-googleIconLinkTag.setAttribute('rel', 'stylesheet');
-googleIconLinkTag.setAttribute('href', "https://fonts.googleapis.com/css2?family=Material+Symbols+" +
-    "Outlined:opsz,wght,FILL,GRAD@24,400,0,0");
-document.getElementsByTagName('head')[0].appendChild(googleIconLinkTag);
-
-
-
 const WAYBACK_ENDPORT = "http://archive.org/wayback/available?url=";
 const ARCHIVED_SNAPSHOTS = "archived_snapshots";
 const CLOSEST = "closest";
@@ -41,6 +29,9 @@ const URL = "url";
 
 (async function() {
     'use strict';
+    const iconCSS = GM_getResourceText("MATERIAL_ICON");
+    GM_addStyle(iconCSS);
+
     if (await is404Page(window.location.href)) {
         createInfoBar("This page is missing. Do you want to check for a saved version on the Wayback Machine?",
             "Check for saved version", () => hasSavedVersion(window.location.href),
@@ -52,38 +43,70 @@ const URL = "url";
 })();
 
 async function is404Page(url) {
+    try {
+        const options = {
+            url: url,
+            method: 'head'
+        }
 
-    const res = await fetch(url, { method: 'head'})
-    const errorCodes = [404, 408, 410, 451, 500, 502, 503, 509, 520, 521, 523, 524, 525, 526];
+        const res = await GM.xmlHttpRequest(options);
+        const errorCodes = [404, 408, 410, 451, 500, 502, 503, 509, 520, 521, 523, 524, 525, 526];
 
-    return errorCodes.includes(res.status);
+        return errorCodes.includes(res.status);
+    } catch (err) {
+        return false;
+    }
 
 
 }
 
 async function hasSavedVersion(currentURL) {
+    try {
 
-    const options = {
-        headers: {
-            "Content-type": "application/json"
+        buttonNode.className = "material-symbols-outlined";
+        buttonNode.textContent = "progress_activity";
+        buttonNode.animate([
+                { transform: 'rotate(0deg)' },
+                { transform: 'rotate(360deg)'},
+            ],
+            {
+                duration: 2000,
+                iterations: Infinity
+            });
+
+        const options = {
+            method: 'GET',
+            url: WAYBACK_ENDPORT + currentURL,
+            headers: {
+                "Content-type": "application/json"
+            },
+            responseType: 'JSON'
+        };
+
+
+
+        let res = await GM.xmlHttpRequest(options);
+        let archive = await res.response;
+
+        if (Object.keys(archive[ARCHIVED_SNAPSHOTS]).length === 0) {
+            removeInfoBar();
+            createInfoBar("No saved version of this page has been found on the Wayback machine",
+                null, null, false);
+            return false;
+        } else {
+            window.location.href = archive[ARCHIVED_SNAPSHOTS][CLOSEST][URL];
+
         }
-    };
-
-    let res = await fetch(WAYBACK_ENDPORT + currentURL, options);
-    let archive = await res.json();
-
-    if (Object.keys(archive[ARCHIVED_SNAPSHOTS]).length === 0) {
+    } catch(err) {
         removeInfoBar();
-        createInfoBar("No saved version of this page has been found on the Wayback machine",
-            null, null, false);
-        return false;
-    } else {
-        window.location.href = archive[ARCHIVED_SNAPSHOTS][CLOSEST][URL];
-
+        createInfoBar("Sorry. Unable to get data from the Wayback machine", null, null, false);
     }
 
 
+
 }
+
+const buttonNode = document.createElement('button');
 
 function createInfoBar(infoBarText, buttonTextContent, onBtnClick, createButton) {
 
@@ -97,12 +120,10 @@ function createInfoBar(infoBarText, buttonTextContent, onBtnClick, createButton)
     infoBarContainerNode.appendChild(infoBarTextNode);
 
     if (createButton) {
-        const buttonNode = document.createElement('button');
         buttonNode.setAttribute('style', buttonStyle);
         buttonNode.textContent = buttonTextContent;
         buttonNode.addEventListener('click', onBtnClick);
         infoBarContainerNode.appendChild(buttonNode);
-
 
     }
 
