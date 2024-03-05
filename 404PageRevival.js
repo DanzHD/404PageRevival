d65938c610b5ab8a92a82bf9edd8dd1e0c4f3b89
Daniel Duong <daniel.d.cow@gmail.com>
2024-03-05 16:52:38 +1100

    Added heuristic for 404 page detection and added ability to creeate info bar from the tampermonkey menu

--- 404PageRevival.js
+++ 404PageRevival.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         404PageRevival
 // @namespace    http://tampermonkey.net/
-// @version      1.1.3
+// @version      1.3.0
 // @description  Redirects 404 page not found to the according page in the Wayback Machine
 // @author       Daniel Duong
 // @match        *://*/*
@@ -9,6 +9,7 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
+// @grant        GM_registerMenuCommand
 // @resource     MATERIAL_ICON https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined
 // @license      MIT
 
@@ -19,8 +20,8 @@ const infoBarContainerStyle = 'position: absolute; min-width: 450px; width: 100v
     'border-box; font-family: open-sans; justify-content: space-between; font-size: 20px; '
 const buttonStyle = "position: relative; min-width: fit-content; height: 80%; background-color: orange; vertical-align: " +
     "center; display: inline-flex; justify-content: center; align-items: center; border-radius: 15px; border: none; " +
-    "color: white; font-size: 20px; padding: 10px; margin-left: auto; margin-right: 5%; cursor: pointer; overflow: hidden; white-space: nowrap; font-family: Arial !important; text-transform: none !important; font-weight: 500 !important; ";
-const textStyle = `color: white; white-space: nowrap; text-overflow: ellipsis; overflow: hidden; margin-right: 1%; margin-bottom: 0; margin-top: 0; font-family: Arial !important; text-transform: none !important; font-size: 20px !important; font-weight: 500 !important;`;
+    "color: white; font-size: 20px; padding: 10px; margin-left: auto; margin-right: 5%; cursor: pointer; overflow: hidden; white-space: nowrap; font-family: 'Segoe UI' !important; text-transform: none !important; font-weight: 500 !important; ";
+const textStyle = `color: white; white-space: nowrap; text-overflow: ellipsis; overflow: hidden; margin-right: 1%; margin-bottom: 0; margin-top: 0; font-family: 'Segoe UI' !important; text-transform: none !important; font-size: 20px !important; font-weight: 500 !important;`;
 
 const WAYBACK_ENDPORT = "http://archive.org/wayback/available?url=";
 const ARCHIVED_SNAPSHOTS = "archived_snapshots";
@@ -29,49 +30,56 @@ const URL = "url";
 
 let infobarCreated = false;
 
+
 (async function() {
     'use strict';
+    if (await is404Page(window.location.href)) {
+        await infobarExecutor();
+
+    }
+    GM_registerMenuCommand('Create Infobar', async function() {
+        await infobarExecutor();
+    }, 'r');
     const iconCSS = GM_getResourceText("MATERIAL_ICON");
     GM_addStyle(iconCSS);
 
-    if (await is404Page(window.location.href)) {
-        try {
+})();
 
-            let savedURL = await hasSavedVersion(window.location.href);
-            if (savedURL) {
-                infobarCreated = true;
-                createInfoBar("Click on the button to redirect to the Wayback Machine", "Redirect to internet archive", () => window.location.href=savedURL, true);
-            } else {
-                createInfoBar("No archive of page found on the Wayback Machine", null, null, false);
-            }
-        } catch(err) {
-            createInfoBar("Something went wrong...", null, null, false);
-        }
-
-
+async function infobarExecutor() {
+    if (infobarCreated) {
+        alert("Infobar has already been created");
+        return;
     }
 
+    try {
 
-
-})();
+        let savedURL = await hasSavedVersion(window.location.href);
+        if (savedURL) {
+            createInfoBar("Click on the button to redirect to the Wayback Machine", "Redirect to internet archive", () => window.location.href=savedURL, true);
+        } else {
+            createInfoBar("No archive of page found on the Wayback Machine", null, null, false);
+        }
+    } catch(err) {
+        createInfoBar("Something went wrong...", null, null, false);
+    }
+}
 
 async function is404Page(url) {
     try {
+
         const options = {
             url: url
         }
-
-
-        const {status} = await GM.xmlHttpRequest(options);
+        const response = await GM.xmlHttpRequest(options);
+        const {status} = response;
 
         const errorCodes = [404, 408, 410, 451, 500, 502, 503, 509, 520, 521, 523, 524, 525, 526];
+        const txt = document.documentElement.innerHTML;
 
-
-        return errorCodes.includes(status);
+        return errorCodes.includes(status) && txt.search('404') !== -1;
     } catch (err) {
         return false;
     }
-
 
 }
 
@@ -140,12 +148,14 @@ function createInfoBar(infoBarText, buttonTextContent, onBtnClick, createButton)
 
     infoBarContainerNode.setAttribute('style', infoBarContainerStyle);
     bodyElement.insertBefore(infoBarContainerNode, bodyElement.firstChild);
+    infobarCreated = true;
 
 
 }
 
 function removeInfoBar() {
     const bodyElement = document.querySelector('body');
+    infobarCreated = false;
     bodyElement.removeChild(bodyElement.firstChild);
 
 }
